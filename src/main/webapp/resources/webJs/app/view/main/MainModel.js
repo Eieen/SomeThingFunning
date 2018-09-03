d399aeb894961ad921ee522ff7a864b86076a40e
hanyi <hanyi@DESKTOP-44QMSTO>
2018-09-03 12:40:32 +0800

    第一次提交

---
+++ src/main/webapp/resources/webJs/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('webJS.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'webJS'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
