1c2772c52d32da01c63679e122c38df6b35dff7c
hanyi <hanyi@DESKTOP-44QMSTO>
2018-09-12 10:37:27 +0800

    添加新结构的Ext

---
+++ src/main/webapp/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('SmthFun.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'SmthFun'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ src/main/webapp/resources/Readme.md
@@ -0,0 +1,4 @@
+# SmthFun/resources
+
+This folder contains resources (such as images) needed by the application. This file can
+be removed if not needed.
